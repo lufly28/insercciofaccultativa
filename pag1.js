6e525542b14c9e3ee09369c2696d722c9da9706f
Flor Gira <florenciagira2007@gmail.com>
2024-09-16 17:49:06 -0300

    actualizacion final

--- pag1.js
+++ pag1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const triggerTabList = document.querySelectorAll('#myTab a')
 triggerTabList.forEach(triggerEl => {
   const tabTrigger = new bootstrap.Tab(triggerEl)
@@ -10,17 +9,4 @@ triggerTabList.forEach(triggerEl => {
 })
 
 const triggerFirstTabEl = document.querySelector('#myTab li:first-child a')
-=======
-const triggerTabList = document.querySelectorAll('#myTab a')
-triggerTabList.forEach(triggerEl => {
-  const tabTrigger = new bootstrap.Tab(triggerEl)
-
-  triggerEl.addEventListener('click', event => {
-    event.preventDefault()
-    tabTrigger.show()
-  })
-})
-
-const triggerFirstTabEl = document.querySelector('#myTab li:first-child a')
->>>>>>> 81eb1a50d1136dcad27cf0b7f47a3dcd0afc36fb
 bootstrap.Tab.getInstance(triggerFirstTabEl).show() // Select first tab
--- script4.js
+++ script4.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
@@ -22,29 +21,4 @@ form.addEventListener('submit', (event) => {
     }
 
   
-=======
-const form = document.querySelector('form');
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const universidad = document.getElementById('universidad').value;
-
-    // Aquí debes agregar la lógica para redireccionar al usuario al chat correspondiente
-    // Por ejemplo:
-    if (universidad === 'uba') {
-        window.location.href = 'https://discord.gg/DCCHakMMsh';
-    } else if (universidad === 'utn') {
-        window.location.href = 'https://discord.gg/DHSUU5gdxQ';
-    } else if (universidad === 'unlam') {
-        window.location.href = 'https://discord.gg/ydrvJkkKFv' ;
-    } else if (universidad === 'unlp') {
-        window.location.href = 'https://discord.gg/k6VdHyQeyX' ;
-    } else if (universidad === 'unsam') {
-        window.location.href = 'https://discord.gg/GJZHUZdUJ6' ;
-    } else if (universidad === 'una')   {
-        window.location.href = 'https://discord.gg/r4djxKJrsN';
-    }
-
-  
->>>>>>> 81eb1a50d1136dcad27cf0b7f47a3dcd0afc36fb
 });
